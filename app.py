40a3b120ea5050f0ed33ff96964868dc04ef7759
heysarver <22250203+heysarver@users.noreply.github.com>
2024-03-01 19:42:33 -0500

    moved

--- app.py
+++ app.py
@@ -16,13 +16,21 @@ def parse_args():
     parser.add_argument("--github-repo", type=str, default=GITHUB_REPO, help="GitHub repository URL")
     parser.add_argument("--local-path", type=str, default=LOCAL_PATH, help="Local path to clone the repository")  
     parser.add_argument("--file-extensions", type=str, default=FILE_EXTENSIONS, help="File extensions to embed, e.g. '.py,.md'")  
-    parser.add_argument("--codebase-name", type=str, default="local", help="Name of the codebase")
+    parser.add_argument("--codebase-name", type=str, default=CODEBASE_NAME, help="Name of the codebase")
 
     args = parser.parse_args()
     if args.github_repo != "" and args.local_path != "":
         raise ValueError("Cannot specify both github-repo and local-path.  Please specify only one.")
 
     return args
+
+def clone_repo(repo_url, dest_folder, remove_dot_git=True):
+    Repo.clone_from(repo_url, dest_folder)
+    if remove_dot_git:
+        shutil.rmtree(f"{dest_folder}/.git")
+
+def get_repo_name(repo_url):
+    return repo_url.split("/")[-1].replace(".git", "")
 
 class App:
     def __init__(self, args):
@@ -32,20 +40,9 @@ class App:
         self.github_repo = args.github_repo
 
         if self.github_repo:
-            self.clone_repo(self.github_repo, self.source_dir)
-            self.codebase_name = self.codebase_name or self.get_repo_name(self.github_repo)
-
-    @staticmethod
-    def clone_repo(repo_url, dest_folder, remove_dot_git=True):
-        Repo.clone_from(repo_url, dest_folder)
-        if remove_dot_git:
-            shutil.rmtree(f"{dest_folder}/.git")
-
-    @staticmethod
-    def get_repo_name(repo_url):
-        return repo_url.split("/")[-1].replace(".git", "")
+            clone_repo(self.github_repo, self.source_dir)
+            self.codebase_name = self.codebase_name or get_repo_name(self.github_repo)
 
 if __name__ == "__main__":
     args = parse_args()
     app = App(args)
-    
