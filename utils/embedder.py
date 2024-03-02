03fdd0c1a12a8fa35fe58c376adcbeb1cec20be6
heysarver <22250203+heysarver@users.noreply.github.com>
2024-03-01 22:30:04 -0500

    this should work :(

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,26 +1,17 @@
 from langchain.text_splitter import CharacterTextSplitter
-# from utils.file_loader import load_files
-from langchain_community.document_loaders import TextLoader
-
-def load_files(file_list):
-    docs = []
-    for file in file_list:
-        loader = TextLoader(file)
-        docs.extend(loader.load())
-        print(f'Loaded {len(docs)} documents from {file}')  # Debug output
-    return docs
+from utils.file_loader import load_files
 
 def embed_files(file_list, provider, model):
     docs = load_files(file_list)
     print(f'Total documents to embed: {len(docs)}')  # Debug output
-    # print(docs)
+    print(type(docs))
+    print(type(docs[0]))
     chunks = split_docs(docs)
 
 def split_docs(docs, chunk_size=1000, overlap=100):
     text_splitter = CharacterTextSplitter(chunk_size, overlap)
-    chunked_documents = []
-    for doc in docs:
-        # print(doc.page_content)
-        chunks = text_splitter.split_documents(doc)
-        chunked_documents.extend(chunks)
+    #chunked_documents = []
+
+    chunks = text_splitter.split_documents(docs)
+    #chunked_documents.extend(chunks)
     # print(f'Total chunks: {len(chunked_documents)}')  # Debug output
