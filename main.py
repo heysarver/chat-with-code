980a268814b8a9f26df27d8dba7c548c868d24b0
heysarver <22250203+heysarver@users.noreply.github.com>
2024-03-02 21:13:10 -0500

    save, pre-skew-ai

--- main.py
+++ main.py
@@ -1,10 +1,80 @@
-from utils.arg_parser import parse_args
 from app.app import App
-from utils.embedder import embed_files
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+import argparse
+import os
+from dotenv import load_dotenv
+
+def parse_args():
+    load_dotenv()
+
+    GITHUB_REPO = os.getenv("GITHUB_REPO", "")
+    LOCAL_PATH = os.getenv("LOCAL_PATH", "")
+    FILE_EXTENSIONS = os.getenv("FILE_EXTENSIONS", ".py,.md")
+    CODEBASE_NAME = os.getenv("CODEBASE_NAME", "local")
+    ELASTICSEARCH_INDEX = os.getenv("ELASTICSEARCH_INDEX", CODEBASE_NAME + "_vectors")
+    ELASTICSEARCH_SCHEME = os.getenv("ELASTICSEARCH_SCHEME", "https")
+    ELASTICSEARCH_HOST = os.getenv("ELASTICSEARCH_HOST", "localhost")
+    ELASTICSEARCH_PORT = os.getenv("ELASTICSEARCH_PORT", 9200)
+    ELASTICSEARCH_VERIFY_CERTS = os.getenv("ELASTICSEARCH_VERIFY_CERTS", False)
+    ELASTICSEARCH_USERNAME = os.getenv("ELASTICSEARCH_USERNAME", "elastic")
+    ELASTICSEARCH_PASSWORD = os.getenv("ELASTICSEARCH_PASSWORD", "password")
+    ELASTICSEARCH_DELETE_INDEX = os.getenv("ELASTICSEARCH_DELETE_INDEX", True)
+    EMBEDDING_PROVIDER = os.getenv("EMBEDDING_PROVIDER", "huggingface")
+    EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "bigcode/starcoder2-3b")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+    CHUNK_LENGTH = os.getenv("CHUNK_LENGTH", 1024)
+    CHUNK_OVERLAP = os.getenv("CHUNK_OVERLAP", 128)
+    SEARCH_RESULTS = os.getenv("SEARCH_RESULTS", 50)
+    SEARCH_MATCH_PERCENTAGE = os.getenv("SEARCH_MATCH_PERCENTAGE", 0.8)
+    SKIP_EMBEDDING = os.getenv("SKIP_EMBEDDING", False)
+
+    parser = argparse.ArgumentParser(description="Process some parameters.")
+    parser.add_argument("--github-repo", type=str, default=GITHUB_REPO, help="GitHub repository URL")
+    parser.add_argument("--local-path", type=str, default=LOCAL_PATH, help="Local path to clone the repository")  
+    parser.add_argument("--file-extensions", type=str, default=FILE_EXTENSIONS, help="File extensions to embed, e.g. '.py,.md'")  
+    parser.add_argument("--codebase-name", type=str, default=CODEBASE_NAME, help="Name of the codebase")
+    parser.add_argument("--elasticsearch-index", type=str, default=ELASTICSEARCH_INDEX, help="Name of the elasticsearch index")
+    parser.add_argument("--elasticsearch-scheme", type=str, default=ELASTICSEARCH_SCHEME, help="Scheme of the elasticsearch host")
+    parser.add_argument("--elasticsearch-host", type=str, default=ELASTICSEARCH_HOST, help="Host of the elasticsearch")
+    parser.add_argument("--elasticsearch-port", type=str, default=ELASTICSEARCH_PORT, help="Port of the elasticsearch")
+    parser.add_argument("--elasticsearch-verify-certs", type=bool, default=ELASTICSEARCH_VERIFY_CERTS, help="Verify certificates of the elasticsearch")
+    parser.add_argument("--elasticsearch-username", type=str, default=ELASTICSEARCH_USERNAME, help="Username of the elasticsearch")
+    parser.add_argument("--elasticsearch-password", type=str, default=ELASTICSEARCH_PASSWORD, help="Password of the elasticsearch")
+    parser.add_argument("--elasticsearch-delete-index", type=bool, default=ELASTICSEARCH_DELETE_INDEX, help="Delete the index before saving embeddings")
+    parser.add_argument("--embedding-provider", type=str, default=EMBEDDING_PROVIDER, help="Provider for embedding")
+    parser.add_argument("--embedding-model", type=str, default=EMBEDDING_MODEL, help="Model for embedding")
+    parser.add_argument("--openai-api-key", type=str, default=OPENAI_API_KEY, help="OpenAI API key")
+    parser.add_argument("--chunk-length", type=int, default=CHUNK_LENGTH, help="Length of the chunk")
+    parser.add_argument("--chunk-overlap", type=int, default=CHUNK_OVERLAP, help="Overlap of the chunk")
+    parser.add_argument("--search-results", type=int, default=SEARCH_RESULTS, help="Number of search results")
+    parser.add_argument("--search-match-percentage", type=float, default=SEARCH_MATCH_PERCENTAGE, help="Match percentage for search")
+    parser.add_argument("--skip-embedding", type=bool, default=SKIP_EMBEDDING, help="Skip embedding")
+
+    args = parser.parse_args()
+    if args.github_repo != "" and args.local_path != "":
+        raise ValueError("Cannot specify both github-repo and local-path.  Please specify only one.")
+
+    return args
+
+def print_search_results(search_result):
+    hits = search_result['hits']['hits']
+    for i, hit in enumerate(hits, start=1):
+        source = hit['_source']
+        text = source['text']
+        metadata = source['metadata']
+        score = hit['_score']
+        print(f"Result {i}:")
+        print(f"Score: {score}")
+        print(f"Metadata: {metadata}")
+        print("\n")
 
 if __name__ == "__main__":
     args = parse_args()
     app = App(args)
-    
-    embed_files(app.file_list(), None, None)
-    
+    if not app.skip_embedding:
+        app.create_documents(provider=args.embedding_provider, model=args.embedding_model)
+        app.store_documents()
+    search_result = app.semantic_search(query = "generate sample code for create_transaction") # use the user's query OR use LLM to get keywords?
+    print_search_results(search_result)
--- utils/file_loader.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import TextLoader
-
-def load_files(file_list):
-    files = []
-    for file in file_list:
-        loader = TextLoader(file)
-        files.extend(loader.load())
-    return files
